676a50623a5d6e973876b82f8b0e46e57c05d1a0
Zalina Kodz <kod3ik87@gmail.com>
2023-04-19 13:50:06 +0300

    added pageObject

--- src/test/java/Test/X5GroupTests.java
+++ src/test/java/Test/X5GroupTests.java
@@ -1,41 +1,108 @@
 package Test;
 
+import org.junit.jupiter.api.*;
+import pages.X5Elements;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 @Tag("X5Group")
 public class X5GroupTests extends TestBase {
+    X5Elements x5Elements = new X5Elements();
     @Test
-    @DisplayName("Доступность раздела Новости из вкладки Покупателю ")
-    public void checkNewsTests() {
+    @DisplayName("Проверка, что в разделе 'Карьера' есть описание миссии компании")
+    public void checkSearchResult() {
         step("Открываем главную страницу", () -> {
-            open("https://www.x5.ru/ru/");
+            x5Elements.openPage();
         });
-        step("Скроллим до раздела Покупателю", () -> {
-            $(".block-header__heading-wrap").scrollIntoView(true);
+        step("Кликаем по значку поиска", () -> {
+            x5Elements.clickSearchIcon();
         });
-        step("Выбираем Найти рецепт и подобрать ингредиенты", () -> {
-            $(".buyer__text-content").shouldHave(text("Найти рецепт и подобрать ингредиенты")).click();
+        step("Вводим в поле поиска 'Открытие'", () -> {
+            x5Elements.enterInSearchField("Открытие");
         });
-        step("Скроллим до раздела Новости", () -> {
-            $(".news-block__head").scrollIntoView(true);
+        step("Скроллим страницу до результатов поиска", () -> {
+            x5Elements.scrollToResultOfSearch(true);
         });
-        step("Переходим в раздел Все публикации", () -> {
-            $(".news-block__controlls").shouldHave(text("Все публикации")).click();
+        step("Проверяем, что по поиску находится статья под названием 'История компании'", () -> {
+            x5Elements.checkThatResultExist();
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка, что в разделе 'Карьера' есть описание миссии компании")
+    public void checkСareerSectionTest() {
+        step("Открываем главную страницу", () -> {
+            x5Elements.openPage();
+        });
+        step("Переходим в раздел 'Карьера'", () -> {
+            x5Elements.chooseSectionCareer("Карьера");
+        });
+        step("Скроллим до раздела 'Работа в X5 Group'", () -> {
+            x5Elements.scrollIntoView(true);
+        });
+        step("Проверяем, что на странице есть текст: 'Наша миссия — улучшать качество жизни людей'", () -> {
+            x5Elements.checkTextMission("Наша миссия — улучшать качество жизни людей");
+        });
+    }
+
+    @Test
+    @DisplayName("Доступность раздела 'Новости' из вкладки 'Покупателю'")
+    public void checkNewsSectionTests() {
+        step("Открываем главную страницу", () -> {
+            x5Elements.openPage();
+        });
+        step("Скроллим до раздела 'Покупателю'", () -> {
+            x5Elements.scrollSectionConsumer(true);
+        });
+        step("Выбираем 'Найти рецепт и подобрать ингредиенты'", () -> {
+            x5Elements.choosePlanning("Найти рецепт и подобрать ингредиенты");
+        });
+        step("Скроллим до раздела 'Новости'", () -> {
+            x5Elements.scrollSectionNews(true);
+        });
+        step("Переходим в раздел 'Все публикации'", () -> {
+            x5Elements.clickAllPublication("Все публикации");
         });
         step("Выбираем первую статью в списке", () -> {
-            $("#splide01-slide01").click();
+            x5Elements. getFistPublication();
         });
         step("Проверяем, что пресс-релиз к статье можно скачать", () -> {
-            $(".docslist__docs--tile").scrollIntoView(true);
-            $(".button-link--hover-outer-bg-none").doubleClick();
+            x5Elements.downloadingPressRelease(true);
+        });
+    }
+
+    @Test
+    @DisplayName("Проверка, что в разделе 'Компания' есть описание миссии компании")
+    public void negativeTestForSectionCompany() {
+        step("Открываем главную страницу", () -> {
+            x5Elements.openPage();
+        });
+        step("Переходим в раздел 'Компания'", () -> {
+            x5Elements.chooseSectionCompany();
+        });
+        step("Скроллим до раздела 'Работа в X5 Group'", () -> {
+            x5Elements.scrollWorkingAtX5(true);
+        });
+        step("Проверяем, что тест падает если искать по тексту количество работников != 353 000 ", () -> {
+            x5Elements.checkNumberOfEmployees();
+        });
+    }
+
+    @Test
+    @Disabled
+    @DisplayName("Проверка, что по клику на 'EN' сайт переходит на английский")
+    public void skippedTestCheckingTranslation() {
+        step("Открываем главную страницу", () -> {
+            x5Elements.openPage();
+        });
+        step("Кликаем по кнопке 'EN' в главном меню справа", () -> {
+            x5Elements.clickEnLang();
+        });
+        step("Скроллим до футера", () -> {
+            x5Elements.goToFooter(true);
+        });
+        step("Проверяем, что на адресс компании отображается на английском", () -> {
+            x5Elements.checkAddress("119049, Russia, Moscow, Koroviy Val street, 5, building 1");
         });
     }
 }
---
+++ src/test/java/pages/X5Elements.java
@@ -0,0 +1,160 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Assertions;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class X5Elements {
+    private final SelenideElement
+            searchIcon = $(".header-search__search-btn"),
+            searchInput = $("#search-term-header"),
+            scrollToSearchResult = $(".search-results__content-section"),
+            resultShouldBe = $(withText("История компании")),
+            goToCareer = $(".header--active"),
+            scrollToCareer = $(".text-w-media--reverse"),
+            checkText = $(withText("Наша миссия — улучшать качество жизни людей")),
+            scrollToConsumer = $(".block-header__heading-wrap"),
+            chooseSection = $(".buyer__text-content"),
+            scrollToNews = $(".news-block__head"),
+            goToAllPublication = $(".news-block__controlls"),
+            chooseFirstPublication = $("#splide01-slide01"),
+            scrollToFile = $(".docslist__docs--tile"),
+            downloadFile = $(".button-link-trigger-hover"),
+            goToCompany = $(byText("Компания")),
+            scrollToWorkInX5 = $(".key-numbers__content"),
+            checkNumberInText = $(withText("343000")),
+            chooseLanguage = $(".header__lang"),
+            scrollToFooter = $(".css-ixvcfi-indicatorContainer"),
+            checkAddressInFooter = $(".footer__address");
+
+    public X5Elements openPage() {
+        open("https://www.x5.ru/ru/");
+
+        return this;
+    }
+
+    public X5Elements clickSearchIcon() {
+        searchIcon.click();
+
+        return this;
+    }
+
+    public X5Elements enterInSearchField(String value) {
+        searchInput.setValue(value).pressEnter();
+
+        return this;
+    }
+
+    public X5Elements scrollToResultOfSearch(Boolean value) {
+        scrollToSearchResult.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements checkThatResultExist() {
+        resultShouldBe.should(exist);
+
+        return this;
+    }
+
+    public X5Elements chooseSectionCareer(String value) {
+        goToCareer.shouldHave(text(value)).click();
+
+        return this;
+    }
+
+    public X5Elements scrollIntoView(Boolean value) {
+        scrollToCareer.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements checkTextMission(String value) {
+        checkText.shouldHave(text(value)).should(exist);
+
+        return this;
+    }
+
+    public X5Elements scrollSectionConsumer(Boolean value) {
+        scrollToConsumer.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements choosePlanning(String value) {
+        chooseSection.shouldHave(text(value)).click();
+
+        return this;
+    }
+
+    public X5Elements scrollSectionNews(Boolean value) {
+        scrollToNews.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements clickAllPublication(String value) {
+        goToAllPublication.shouldHave(text(value)).click();
+
+        return this;
+    }
+
+    public X5Elements getFistPublication() {
+        chooseFirstPublication.click();
+
+        return this;
+    }
+
+    public X5Elements downloadingPressRelease(Boolean value) throws FileNotFoundException {
+        scrollToFile.scrollIntoView(value);
+        File file = downloadFile.download();
+        Assertions.assertTrue(file.exists());
+
+        return this;
+    }
+
+    public X5Elements chooseSectionCompany() {
+        goToCompany.click();
+
+        return this;
+    }
+
+    public X5Elements scrollWorkingAtX5(Boolean value) {
+        scrollToWorkInX5.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements checkNumberOfEmployees() {
+        checkNumberInText.should(exist);
+
+        return this;
+    }
+
+    public X5Elements clickEnLang() {
+        chooseLanguage.click();
+
+        return this;
+    }
+
+    public X5Elements goToFooter(Boolean value) {
+        scrollToFooter.scrollIntoView(value);
+
+        return this;
+    }
+
+    public X5Elements checkAddress(String value) {
+        checkAddressInFooter.shouldHave(text(value)).click();
+
+        return this;
+    }
+}
